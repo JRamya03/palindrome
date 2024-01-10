bfef7d044844291b78197706bb0da8e54fabe6c2
Ramya <ramyaj134@gmail.com>
2024-01-10 23:37:44 +0530

    palindrome

---
+++ srcipt.js
@@ -0,0 +1,16 @@
+function convert(){
+    let str1 = document.getElementById("input").value
+      let str2 = str1.split('').reverse().join('')
+   let str3
+    
+      if (str1 == str2){
+          str3 = "The given text is palindrome"
+      }
+
+      
+      else {
+          str3 = " The given text is not a palindrome"
+      }
+   let result= document.getElementById("result")
+   result.innerHTML =  `${str3} `
+ }
